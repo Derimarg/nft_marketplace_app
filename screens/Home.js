6ca9a6bdbac2188812660d4d43013df80eb8e1d4
Derimar Gray <derimargray@gmail.com>
2022-05-04 11:30:12 -0500

    update expo version and add search functionality

--- screens/Home.js
+++ screens/Home.js
@@ -5,7 +5,23 @@ import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
 import { COLORS, NFTData } from "../constants";
 
 const Home = () => {
+  const [nftData, setNftData] = useState(NFTData);
 
+  const handleSearch = (value) => {
+    if (value.length === 0) {
+      setNftData(NFTData);
+    }
+
+    const filteredData = NFTData.filter((item) =>
+      item.name.toLowerCase().includes(value.toLowerCase())
+    );
+
+    if (filteredData.length === 0) {
+      setNftData(NFTData);
+    } else {
+      setNftData(filteredData);
+    }
+  };
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
@@ -13,11 +29,11 @@ const Home = () => {
       <View style={{ flex: 1 }}>
         <View style={{ zIndex: 0 }}>
           <FlatList
-            data={NFTData}
+            data={nftData}
             renderItem={({ item }) => <NFTCard data={item} />}
             keyExtractor={(item) => item.id}
             showsVerticalScrollIndicator={false}
-            ListHeaderComponent={<HomeHeader />}
+            ListHeaderComponent={<HomeHeader onSearch={handleSearch} />}
           />
         </View>
 
